e9f2876aea4e85489c58fae575be5a49703a25ca
Imre Toth <tothimre@designtech.hu>
2024-07-26 15:10:07 +0200

    Added base layout and remove unused old components

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
 <template>
-  <div class="bg-neutral-200"></div>
+  <header class="bg-neutral-100">
+    <div class="container mx-auto py-3">
+      <nav class="flex items-center gap-x-5">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/products">Products</RouterLink>
+        <RouterLink to="/products" class="ml-auto">Cart</RouterLink>
+      </nav>
+    </div>
+  </header>
 </template>
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>ProductsView</div>
+</template>
